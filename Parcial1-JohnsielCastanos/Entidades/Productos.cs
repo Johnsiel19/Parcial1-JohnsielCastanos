9aaf50b4c435fe11a433b10727073e0bdb0808b9
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-05-31 10:02:52 -0400

    Agregando Entidad Prodectos

---
+++ Parcial1-JohnsielCastanos/Entidades/Productos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_JohnsielCastanos.Entidades
+{
+    public class Productos
+    {
+
+        [Key]
+        public int ProductosId { get; set; }
+
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public float Costo { get; set; }
+        public float ValorInvetario { get; set;}
+
+        public Productos()
+        {
+            ProductosId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0.00F;
+            ValorInvetario = 0.00F;
+
+        }
+
+
+    }
+}
