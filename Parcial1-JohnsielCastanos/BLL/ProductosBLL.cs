46e03f4fb65fb5cb14f6c65facbb212741ba3e91
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-05-31 13:49:10 -0400

    Agragando productosBLL y agegando metodo guardar

---
+++ Parcial1-JohnsielCastanos/BLL/ProductosBLL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parcial1_JohnsielCastanos.DAL;
+using Parcial1_JohnsielCastanos.Entidades;
+using System.Data.Entity;
+
+namespace Parcial1_JohnsielCastanos.BLL
+{
+    public class ProductosBLL
+    {
+
+        public static bool Guardar(Productos producto)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.Producto.Add(producto) != null)
+                    paso = db.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+
+
+    }
+}
