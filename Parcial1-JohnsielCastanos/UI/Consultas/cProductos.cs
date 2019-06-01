f1428f6e21d87c6a000ffbaafc3bade7feca74cb
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-05-31 21:34:18 -0400

    agregando eventos de la consulta de productos

--- Parcial1-JohnsielCastanos/UI/Consultas/cProductos.cs
+++ Parcial1-JohnsielCastanos/UI/Consultas/cProductos.cs
@@ -18,13 +18,42 @@ namespace Parcial1_JohnsielCastanos.UI.Consultas
         public cProductos()
         {
             InitializeComponent();
-           
+            FiltrocomboBox.Text ="Todo";
         }
 
         private void Consultarbutton_Click(object sender, EventArgs e)
         {
 
-            
+            var listado = new List<Productos>();
+
+            if (CriteriotextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltrocomboBox.Text)
+                {
+                    case "Todo":
+                        listado = ProductosBLL.GetList(p => true);
+                        break;
+
+                    case "Id":
+                        int id = Convert.ToInt32(CriteriotextBox.Text);
+                        listado = ProductosBLL.GetList(p => p.ProductosId == id);
+                        break;
+
+                    case "Descripcion":
+                        listado = ProductosBLL.GetList(p => p.Descripcion.Contains(CriteriotextBox.Text));
+                        break;
+
+                }
+
+            }
+            else
+            {
+                listado = ProductosBLL.GetList(p => true);
+            }
+
+            ConsultadataGridView.DataSource = null;
+            ConsultadataGridView.DataSource = listado;
+
         }
     }
 }
